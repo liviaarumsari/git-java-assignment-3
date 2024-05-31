1c86132d802b5013816ed88c406ae9a032e66d00
liviaarumsari <13521094@mahasiswa.itb.ac.id>
2024-05-31 11:23:33 +0700

    setup project

---
+++ src/test/java/org/example/javaassignment3/JavaAssignment3ApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.javaassignment3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaAssignment3ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
