f44117ff73dc901c628ccd9bce7c0ba2f6ed4016
liviaarumsari <13521094@mahasiswa.itb.ac.id>
2024-05-31 11:23:43 +0700

    feat: product entity

---
+++ src/main/java/org/example/javaassignment3/entity/Product.java
@@ -0,0 +1,55 @@
+package org.example.javaassignment3.entity;
+
+import jakarta.persistence.*;
+
+import java.math.BigDecimal;
+
+@Entity
+@Table(name = "products")
+public class Product {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column
+    private Long id;
+
+    @Column
+    private String nama;
+
+    @Column
+    private BigDecimal hargaBeli;
+
+    @Column
+    private BigDecimal hargaJual;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNama() {
+        return nama;
+    }
+
+    public void setNama(String nama) {
+        this.nama = nama;
+    }
+
+    public BigDecimal getHargaBeli() {
+        return hargaBeli;
+    }
+
+    public void setHargaBeli(BigDecimal hargaBeli) {
+        this.hargaBeli = hargaBeli;
+    }
+
+    public BigDecimal getHargaJual() {
+        return hargaJual;
+    }
+
+    public void setHargaJual(BigDecimal hargaJual) {
+        this.hargaJual = hargaJual;
+    }
+}
